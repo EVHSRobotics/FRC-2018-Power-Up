6be4e6f00a66d648839c3bd16f9fdf0cb4a975c6
tvottra <tvottra@gmail.com>
2018-01-29 21:17:33 -0800

    Added Neel's kalman filter code and moved ejml lib to libs folder if need be later

---
+++ src/org/usfirst/frc/team2854/robot/filter/FatKalman/KalmanFilterEquation.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2009-2017, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of Efficient Java Matrix Library (EJML).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.usfirst.frc.team2854.robot.filter.FatKalman;
+
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.equation.Equation;
+import org.ejml.equation.Sequence;
+
+/**
+ * Example of how the equation interface can greatly simplify code
+ *
+ * @author Peter Abeles
+ */
+public class KalmanFilterEquation implements KalmanFilter {
+
+    // system state estimate
+    private DMatrixRMaj x,P;
+
+    private Equation eq;
+
+    // Storage for precompiled code for predict and update
+    Sequence predictX,predictP;
+    Sequence updateY,updateK,updateX,updateP;
+
+    @Override
+    public void configure(DMatrixRMaj F, DMatrixRMaj Q, DMatrixRMaj H) {
+        int dimenX = F.numCols;
+
+        x = new DMatrixRMaj(dimenX,1);
+        P = new DMatrixRMaj(dimenX,dimenX);
+
+        eq = new Equation();
+
+        // Provide aliases between the symbolic variables and matrices we normally interact with
+        // The names do not have to be the same.
+        eq.alias(x,"x",P,"P",Q,"Q",F,"F",H,"H");
+
+        // Dummy matrix place holder to avoid compiler errors.  Will be replaced later on
+        eq.alias(new DMatrixRMaj(1,1),"z");
+        eq.alias(new DMatrixRMaj(1,1),"R");
+
+        // Pre-compile so that it doesn't have to compile it each time it's invoked.  More cumbersome
+        // but for small matrices the overhead is significant
+        predictX = eq.compile("x = F*x");
+        predictP = eq.compile("P = F*P*F' + Q");
+
+        updateY = eq.compile("y = z - H*x");
+        updateK = eq.compile("K = P*H'*inv( H*P*H' + R )");
+        updateX = eq.compile("x = x + K*y");
+        updateP = eq.compile("P = P-K*(H*P)");
+    }
+
+    @Override
+    public void setState(DMatrixRMaj x, DMatrixRMaj P) {
+        this.x.set(x);
+        this.P.set(P);
+    }
+
+    @Override
+    public void predict() {
+        predictX.perform();
+        predictP.perform();
+    }
+
+    @Override
+    public void update(DMatrixRMaj z, DMatrixRMaj R) {
+
+        // Alias will overwrite the reference to the previous matrices with the same name
+        eq.alias(z,"z"); eq.alias(R,"R");
+
+        updateY.perform();
+        updateK.perform();
+        updateX.perform();
+        updateP.perform();
+    }
+
+    @Override
+    public DMatrixRMaj getState() {
+        return x;
+    }
+
+    @Override
+    public DMatrixRMaj getCovariance() {
+        return P;
+    }
+}
---
+++ src/org/usfirst/frc/team2854/robot/filter/FatKalman/KalmanFilterSimple.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2009-2017, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of Efficient Java Matrix Library (EJML).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.usfirst.frc.team2854.robot.filter.FatKalman;
+
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.simple.SimpleMatrix;
+
+/**
+ * A Kalman filter implemented using SimpleMatrix.  The code tends to be easier to
+ * read and write, but the performance is degraded due to excessive creation/destruction of
+ * memory and the use of more generic algorithms.  This also demonstrates how code can be
+ * seamlessly implemented using both SimpleMatrix and DMatrixRMaj.  This allows code
+ * to be quickly prototyped or to be written either by novices or experts.
+ *
+ * @author Peter Abeles
+ */
+public class KalmanFilterSimple implements KalmanFilter {
+
+    // kinematics description
+    private SimpleMatrix F,Q,H;
+
+    // sytem state estimate
+    private SimpleMatrix x,P;
+
+    @Override
+    public void configure(DMatrixRMaj F, DMatrixRMaj Q, DMatrixRMaj H) {
+        this.F = new SimpleMatrix(F);
+        this.Q = new SimpleMatrix(Q);
+        this.H = new SimpleMatrix(H);
+    }
+
+    @Override
+    public void setState(DMatrixRMaj x, DMatrixRMaj P) {
+        this.x = new SimpleMatrix(x);
+        this.P = new SimpleMatrix(P);
+    }
+
+    @Override
+    public void predict() {
+        // x = F x
+        x = F.mult(x);
+
+        // P = F P F' + Q
+        P = F.mult(P).mult(F.transpose()).plus(Q);
+    }
+
+    @Override
+    public void update(DMatrixRMaj _z, DMatrixRMaj _R) {
+        // a fast way to make the matrices usable by SimpleMatrix
+        SimpleMatrix z = SimpleMatrix.wrap(_z);
+        SimpleMatrix R = SimpleMatrix.wrap(_R);
+
+        // y = z - H x
+        SimpleMatrix y = z.minus(H.mult(x));
+
+        // S = H P H' + R
+        SimpleMatrix S = H.mult(P).mult(H.transpose()).plus(R);
+
+        // K = PH'S^(-1)
+        SimpleMatrix K = P.mult(H.transpose().mult(S.invert()));
+
+        // x = x + Ky
+        x = x.plus(K.mult(y));
+
+        // P = (I-kH)P = P - KHP
+        P = P.minus(K.mult(H).mult(P));
+    }
+
+    @Override
+    public DMatrixRMaj getState() {
+        return x.getMatrix();
+    } 
+
+    @Override
+    public DMatrixRMaj getCovariance() {
+        return P.getMatrix();
+    }
+}
